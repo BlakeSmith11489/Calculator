be0e0731f85d1c7c4524fe1a1dfbc25e522e2e87
Blake Smith <suicideking2525@gmail.com>
2019-08-21 17:54:29 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+            Console.WriteLine(num);
+            Console.ReadKey();
+            Console.WriteLine("I am retarded.");
+            Console.ReadKey();
+            Console.WriteLine("I am actually starting to think I have autism. This is terrible....");
+            Console.ReadKey();
+
+        }
+    }
+}
